540e677550bb44a272e78351e6f6503792d44a65
Test-Net009 <nikhil.shelke@quickheal.com>
2025-08-18 17:36:15 +0530

    Add files via upload
    New changes 18/08 version 2

--- seqrite-config.js
+++ seqrite-config.js
@@ -2,7 +2,7 @@
 let userLang = (navigator.language || (navigator).userLanguage || 'en').slice(0, 2);
 window.klaroConfig = {
   "consentManagerId": "68a311e69c450e1ee5a80913",
-  "uuid": "dd5ab577-5e6c-4233-9e76-f74727726d9c",
+  "uuid": "8ddf0d0d-c82a-4f67-90ba-97f2ac7b8314",
   "domain": "https://demo-shp.qhtpl.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
@@ -16,7 +16,330 @@ window.klaroConfig = {
   "hideDeclineAll": false,
   "hideLearnMore": false,
   "disablePoweredBy": false,
-  "translations": {},
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a><u><br><br>Created by Tanmay</u></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
+        },
+        "Others": {
+          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "en": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a><u><br><br>Created by Tanmay</u></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
+        },
+        "Others": {
+          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "zz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a><u><br><br>Created by Tanmay</u></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
+        },
+        "Others": {
+          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "hindi": {
+      "acceptAll": "सभी स्वीकृत",
+      "accept": "स्वीकार करना",
+      "decline": "सभी अस्वीकार करें",
+      "acceptSelected": "चयनित स्वीकार करें",
+      "ok": "सभी स्वीकृत",
+      "consentNotice": {
+        "description": "<p><strong>हम आपकी गोपनीयता का सम्मान करते हैं</strong></p><p> हम आवश्यक साइट कार्यक्षमता, साथ ही मार्केटिंग, वैयक्तिकरण और विश्लेषण को सक्षम करने के लिए कुकीज़ संग्रहीत करते हैं। &quot;सभी स्वीकार करें&quot; पर क्लिक करके, आप कुकीज़ के हमारे उपयोग के लिए सहमति देते हैं। आप अपनी सेटिंग कभी भी बदल सकते हैं। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी नीति।</u></a> <u><br><br>तन्मय द्वारा निर्मित</u></p>",
+        "learnMore": "अनुकूलित करें",
+        "changeDescription": " "
+      },
+      "save": "स्वीकार करना",
+      "consentModal": {
+        "title": "सहमति प्राथमिकताएँ अनुकूलित करें",
+        "description": "हम कुकीज़ का इस्तेमाल आपको पृष्ठों के बीच कुशलतापूर्वक नेविगेट करने, आपकी प्राथमिकताओं को संग्रहीत करने और वेबसाइट के आपके अनुभव को बेहतर बनाने में मदद करने के लिए करते हैं। हालाँकि, हम आपकी गोपनीयता के अधिकार को स्वीकार करते हैं और इसलिए आप कुछ प्रकार की कुकीज़ की अनुमति नहीं देने का विकल्प चुन सकते हैं। अधिक जानकारी प्राप्त करने और हमारी डिफ़ॉल्ट सेटिंग्स बदलने के लिए विभिन्न श्रेणी शीर्षकों पर क्लिक करें। कृपया ध्यान दें कि कुछ प्रकार की कुकीज़ को ब्लॉक करने से साइट के आपके अनुभव पर असर पड़ सकता है। ",
+        "buttons": {
+          "save": "स्वीकार करना",
+          "acceptAll": "सभी स्वीकृत",
+          "decline": "सभी अस्वीकार करें"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>ये कुकीज़ हमें वेबसाइट ट्रैफ़िक, उपयोगकर्ता व्यवहार और जुड़ाव पैटर्न का विश्लेषण करने में मदद करती हैं। एकत्रित जानकारी हमें सामग्री को अनुकूलित करने, उपयोगिता बढ़ाने और समग्र साइट प्रदर्शन में सुधार करने में मदद करती है।</p>"
+        },
+        "Others": {
+          "description": "<p>इस श्रेणी में वे कुकीज़ शामिल हैं जो पूर्वनिर्धारित श्रेणियों में फिट नहीं होती हैं, लेकिन कुछ वेबसाइट कार्यक्षमताओं या सुधारों के लिए आवश्यक हो सकती हैं।</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश्य"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    },
+    "hi": {
+      "acceptAll": "सभी स्वीकृत",
+      "accept": "स्वीकार करना",
+      "decline": "सभी अस्वीकार करें",
+      "acceptSelected": "चयनित स्वीकार करें",
+      "ok": "सभी स्वीकृत",
+      "consentNotice": {
+        "description": "<p><strong>हम आपकी गोपनीयता का सम्मान करते हैं</strong></p><p> हम आवश्यक साइट कार्यक्षमता, साथ ही मार्केटिंग, वैयक्तिकरण और विश्लेषण को सक्षम करने के लिए कुकीज़ संग्रहीत करते हैं। &quot;सभी स्वीकार करें&quot; पर क्लिक करके, आप कुकीज़ के हमारे उपयोग के लिए सहमति देते हैं। आप अपनी सेटिंग कभी भी बदल सकते हैं। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी नीति।</u></a> <u><br><br>तन्मय द्वारा निर्मित</u></p>",
+        "learnMore": "अनुकूलित करें",
+        "changeDescription": " "
+      },
+      "save": "स्वीकार करना",
+      "consentModal": {
+        "title": "सहमति प्राथमिकताएँ अनुकूलित करें",
+        "description": "हम कुकीज़ का इस्तेमाल आपको पृष्ठों के बीच कुशलतापूर्वक नेविगेट करने, आपकी प्राथमिकताओं को संग्रहीत करने और वेबसाइट के आपके अनुभव को बेहतर बनाने में मदद करने के लिए करते हैं। हालाँकि, हम आपकी गोपनीयता के अधिकार को स्वीकार करते हैं और इसलिए आप कुछ प्रकार की कुकीज़ की अनुमति नहीं देने का विकल्प चुन सकते हैं। अधिक जानकारी प्राप्त करने और हमारी डिफ़ॉल्ट सेटिंग्स बदलने के लिए विभिन्न श्रेणी शीर्षकों पर क्लिक करें। कृपया ध्यान दें कि कुछ प्रकार की कुकीज़ को ब्लॉक करने से साइट के आपके अनुभव पर असर पड़ सकता है। ",
+        "buttons": {
+          "save": "स्वीकार करना",
+          "acceptAll": "सभी स्वीकृत",
+          "decline": "सभी अस्वीकार करें"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>ये कुकीज़ हमें वेबसाइट ट्रैफ़िक, उपयोगकर्ता व्यवहार और जुड़ाव पैटर्न का विश्लेषण करने में मदद करती हैं। एकत्रित जानकारी हमें सामग्री को अनुकूलित करने, उपयोगिता बढ़ाने और समग्र साइट प्रदर्शन में सुधार करने में मदद करती है।</p>"
+        },
+        "Others": {
+          "description": "<p>इस श्रेणी में वे कुकीज़ शामिल हैं जो पूर्वनिर्धारित श्रेणियों में फिट नहीं होती हैं, लेकिन कुछ वेबसाइट कार्यक्षमताओं या सुधारों के लिए आवश्यक हो सकती हैं।</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश्य"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    },
+    "marathi": {
+      "acceptAll": "सर्व स्वीकारा",
+      "accept": "स्वीकारा",
+      "decline": "सर्व नाकारा",
+      "acceptSelected": "निवडलेले स्वीकारा",
+      "ok": "सर्व स्वीकारा",
+      "consentNotice": {
+        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही आवश्यक साइट कार्यक्षमता, तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a> <u><br><br>तन्मय यांनी तयार केले</u></p>",
+        "learnMore": "सानुकूलित करा",
+        "changeDescription": " "
+      },
+      "save": "स्वीकारा",
+      "consentModal": {
+        "title": "संमती प्राधान्ये कस्टमाइझ करा",
+        "description": "आम्ही कुकीजचा वापर तुम्हाला पृष्ठांमध्ये कार्यक्षमतेने नेव्हिगेट करण्यास, तुमच्या पसंती संग्रहित करण्यास आणि सामान्यतः वेबसाइटवरील तुमचा अनुभव सुधारण्यास मदत करण्यासाठी करतो. तथापि, आम्ही तुमचा गोपनीयतेचा अधिकार मान्य करतो आणि म्हणून तुम्ही काही प्रकारच्या कुकीजना परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आमच्या डीफॉल्ट सेटिंग्ज बदलण्यासाठी वेगवेगळ्या श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात ठेवा की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने साइटवरील तुमच्या अनुभवावर परिणाम होऊ शकतो. ",
+        "buttons": {
+          "save": "स्वीकारा",
+          "acceptAll": "सर्व स्वीकारा",
+          "decline": "सर्व नाकारा"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>या कुकीज आम्हाला वेबसाइट ट्रॅफिक, वापरकर्त्याचे वर्तन आणि प्रतिबद्धता नमुन्यांचे विश्लेषण करण्यास मदत करतात. गोळा केलेल्या अंतर्दृष्टी आम्हाला सामग्री ऑप्टिमाइझ करण्यास, वापरण्यायोग्यता वाढविण्यास आणि एकूण साइट कार्यप्रदर्शन सुधारण्यास अनुमती देतात.</p>"
+        },
+        "Others": {
+          "description": "<p>या श्रेणीमध्ये अशा कुकीज समाविष्ट आहेत ज्या पूर्वनिर्धारित श्रेणींमध्ये बसत नाहीत परंतु काही वेबसाइट कार्यक्षमता किंवा सुधारणांसाठी आवश्यक असू शकतात.</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    },
+    "mr": {
+      "acceptAll": "सर्व स्वीकारा",
+      "accept": "स्वीकारा",
+      "decline": "सर्व नाकारा",
+      "acceptSelected": "निवडलेले स्वीकारा",
+      "ok": "सर्व स्वीकारा",
+      "consentNotice": {
+        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही आवश्यक साइट कार्यक्षमता, तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a> <u><br><br>तन्मय यांनी तयार केले</u></p>",
+        "learnMore": "सानुकूलित करा",
+        "changeDescription": " "
+      },
+      "save": "स्वीकारा",
+      "consentModal": {
+        "title": "संमती प्राधान्ये कस्टमाइझ करा",
+        "description": "आम्ही कुकीजचा वापर तुम्हाला पृष्ठांमध्ये कार्यक्षमतेने नेव्हिगेट करण्यास, तुमच्या पसंती संग्रहित करण्यास आणि सामान्यतः वेबसाइटवरील तुमचा अनुभव सुधारण्यास मदत करण्यासाठी करतो. तथापि, आम्ही तुमचा गोपनीयतेचा अधिकार मान्य करतो आणि म्हणून तुम्ही काही प्रकारच्या कुकीजना परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आमच्या डीफॉल्ट सेटिंग्ज बदलण्यासाठी वेगवेगळ्या श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात ठेवा की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने साइटवरील तुमच्या अनुभवावर परिणाम होऊ शकतो. ",
+        "buttons": {
+          "save": "स्वीकारा",
+          "acceptAll": "सर्व स्वीकारा",
+          "decline": "सर्व नाकारा"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>या कुकीज आम्हाला वेबसाइट ट्रॅफिक, वापरकर्त्याचे वर्तन आणि प्रतिबद्धता नमुन्यांचे विश्लेषण करण्यास मदत करतात. गोळा केलेल्या अंतर्दृष्टी आम्हाला सामग्री ऑप्टिमाइझ करण्यास, वापरण्यायोग्यता वाढविण्यास आणि एकूण साइट कार्यप्रदर्शन सुधारण्यास अनुमती देतात.</p>"
+        },
+        "Others": {
+          "description": "<p>या श्रेणीमध्ये अशा कुकीज समाविष्ट आहेत ज्या पूर्वनिर्धारित श्रेणींमध्ये बसत नाहीत परंतु काही वेबसाइट कार्यक्षमता किंवा सुधारणांसाठी आवश्यक असू शकतात.</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    }
+  },
   "styling": {
     "theme": [
       "light",
